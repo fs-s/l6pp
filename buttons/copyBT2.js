bbefc1f9e1420ca3e9ce072b2f9a1c405d4e15a0
ratsepmarkus <markusratsep11@gmail.com>
2022-04-12 22:35:46 +0300

    fix

--- buttons/copyBT2.js
+++
@@ -1,7 +0,0 @@
-const textInput = document.getElementById('input');
-const copyButton = document.getElementById('copy');
-
-copyButton.addEventListener('click', () => {
-    textInput.select();
-    document.execCommand('copy');
-});
